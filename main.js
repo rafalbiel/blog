4af5c13f5734ca465fab5cead766e2a7a3f286b8
unknown <rafalbiel77@gmail.com>
2022-11-10 20:07:24 +0100

    divided setions

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let posts = [];
 posts[0] = {
 	id: 1,
 	title: "test1",
-	body: "jakiś tekst w poscie 1",
+	body: "jakiś tekst w poscie 1 ",
 	likesCount: 0,
 };
 posts[1] = {
@@ -19,13 +19,29 @@ posts[2] = {
 	likesCount: 0,
 };
 
-const rootElement = document.querySelector("#root");
+function renderContent() {
+	const rootElement = document.querySelector("#root");
 
-rootElement.innerHTML = "";
+	rootElement.innerHTML = "";
 
-posts.forEach((post) => {
-	const newLabelElement = document.createElement("label");
-	newLabelElement.innerHTML =
-		"post Id:" + post.id + "<br>" + post.title + "  " + post.body + "<br>";
-	rootElement.appendChild(newLabelElement);
-});
+	posts.forEach((post) => {
+		const newLabelPostIdElement = document.createElement("label");
+		newLabelPostIdElement.innerHTML = "post Id: " + post.id + "<br>";
+
+		const newLabelPostTitleElement = document.createElement("label");
+		newLabelPostTitleElement.innerHTML = "post Title: " + post.title + "<br>";
+
+		const newLabelPostBodyElement = document.createElement("label");
+		newLabelPostIdElement.innerHTML = "post body: " + post.body + "<br>";
+
+		const newLabelPostLikesElement = document.createElement("label");
+		newLabelPostLikesElement.innerHTML =
+			"post Likes: " + post.likesCount + "<br><br><br>";
+
+		rootElement.appendChild(newLabelPostIdElement);
+		rootElement.appendChild(newLabelPostTitleElement);
+		rootElement.appendChild(newLabelPostBodyElement);
+		rootElement.appendChild(newLabelPostLikesElement);
+	});
+}
+renderContent();
