c1d1853ab101a26ff2f3c41379adf13cc1e74a23
unknown <rafalbiel77@gmail.com>
2022-11-22 20:44:17 +0100

    added login funcionality

--- login.js
+++ login.js
@@ -1,10 +1,23 @@
-function loginDataLoad() {
+let doesUserExist = false;
+function loginDataCheck() {
 	const emailLoginInput = document.querySelector("#email");
 	const emailLoginValue = emailLoginInput.value;
 
 	const passLoginInput = document.querySelector("#pass");
 	const passLoginValue = passLoginInput.value;
 
-	console.log(emailLoginValue);
-	console.log(passLoginValue);
+	const storedUsersList = JSON.parse(
+		window.localStorage.getItem("registratedUsersList")
+	);
+	console.log(storedUsersList);
+
+	storedUsersList.forEach((element) => {
+		if (element.email === emailLoginValue && element.pass === passLoginValue) {
+			return (doesUserExist = true);
+		}
+	});
+
+	if (doesUserExist === true) {
+		window.location.replace("index-reader.html");
+	}
 }
--- registration.js
+++ registration.js
@@ -1,24 +1,29 @@
-function SignupLoadData(){
-    const nicknameInput = document.querySelector("#nickname");
-    const nicknameValue = nicknameInput.value; 
-    
-    const emailLoginInput = document.querySelector("#email");
+const usersList = [];
+
+function SignupLoadData() {
+	const nicknameInput = document.querySelector("#nickname");
+	const nicknameValue = nicknameInput.value;
+
+	const emailLoginInput = document.querySelector("#email");
 	const emailLoginValue = emailLoginInput.value;
 
 	const passLoginInput = document.querySelector("#pass");
 	const passLoginValue = passLoginInput.value;
 
 	console.log(nicknameValue);
-    console.log(emailLoginValue);
+	console.log(emailLoginValue);
 	console.log(passLoginValue);
 
+	const user = {
+		id: usersList.length,
+		nickname: nicknameValue,
+		email: emailLoginValue,
+		pass: passLoginValue,
+	};
+	usersList.push(user);
 
-    const user = {
-        nickname: nicknameValue,
-        email: emailLoginValue,
-        pass:passLoginValue
-      } 
-      
-      window.localStorage.setItem('newUser', JSON.stringify(user));
-
+	window.localStorage.setItem(
+		"registratedUsersList",
+		JSON.stringify(usersList)
+	);
 }
